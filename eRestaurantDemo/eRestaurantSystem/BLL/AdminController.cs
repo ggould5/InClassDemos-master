904b76e60174b91096bdafe74df5915cc3d6ef53
Gregory Gould <ggould5@studentmail.nait.ca>
2015-09-28 16:53:03 -0600

    Pulled down newest version from master

---
+++ eRestaurantDemo/eRestaurantSystem/BLL/AdminController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using eRestaurantSystem.DAL;
+using eRestaurantSystem.DAL.Entities;
+using System.ComponentModel; //Object Data Source
+#endregion
+
+namespace eRestaurantSystem.BLL
+{
+    [DataObject]
+    public class AdminController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<SpecialEvent> SpecialEvents_List()
+        {
+            //connect to our DbContext class in the DAL
+            //create an instance of the class
+            //we will use a transaction to hold our query
+            using (var context = new eRestaurantContext())
+            {
+                //method syntax
+               // return context.SpecialEvents.OrderBy(x => x.Description).ToList();
+
+                //query syntax
+                var results = from item in context.SpecialEvents
+                              orderby item.Description
+                              select item;
+                return results.ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<Reservation> GetReservationsBySpeicalEvent(string eventcode)
+        {
+            //connect to our DbContext class in the DAL
+            //create an instance of the class
+            //we will use a transaction to hold our query
+            using (var context = new eRestaurantContext())
+            {
+                var results = from item in context.Reservations
+                              where item.EventCode.Equals(eventcode)
+                              orderby item.CustomerName, item.ReservationDate
+                              select item;
+                return results.ToList();
+            }
+        }
+    }
+}
