b61c21711fa59c8a13c7f2978026b31310fa1d5f
Gregory Gould <ggould5@studentmail.nait.ca>
2015-09-29 15:15:51 -0600

    Pulled latest master version from online

--- eRestaurantDemo/eRestaurantSystem/BLL/AdminController.cs
+++ eRestaurantDemo/eRestaurantSystem/BLL/AdminController.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 #region Additional Namespaces
 using eRestaurantSystem.DAL;
 using eRestaurantSystem.DAL.Entities;
+using eRestaurantSystem.DAL.DTOs;
+using eRestaurantSystem.DAL.POCOs;
 using System.ComponentModel; //Object Data Source
 #endregion
 
@@ -24,7 +26,7 @@ namespace eRestaurantSystem.BLL
             using (var context = new eRestaurantContext())
             {
                 //method syntax
-               // return context.SpecialEvents.OrderBy(x => x.Description).ToList();
+                //return context.SpecialEvents.OrderBy(x => x.Description).ToList();
 
                 //query syntax
                 var results = from item in context.SpecialEvents
@@ -35,13 +37,11 @@ namespace eRestaurantSystem.BLL
         }
 
         [DataObjectMethod(DataObjectMethodType.Select, false)]
-        public List<Reservation> GetReservationsBySpeicalEvent(string eventcode)
+        public List<Reservation> GetReservationsByEventCode(string eventcode)
         {
-            //connect to our DbContext class in the DAL
-            //create an instance of the class
-            //we will use a transaction to hold our query
             using (var context = new eRestaurantContext())
             {
+                //query syntax
                 var results = from item in context.Reservations
                               where item.EventCode.Equals(eventcode)
                               orderby item.CustomerName, item.ReservationDate
@@ -49,5 +49,39 @@ namespace eRestaurantSystem.BLL
                 return results.ToList();
             }
         }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<ReservationByDate> GetReservationsByDate(string reservationdate)
+        {
+            using (var context = new eRestaurantContext())
+            {
+                int theYear =(DateTime.Parse(reservationdate)).Year;
+                int theMonth =(DateTime.Parse(reservationdate)).Month;
+                int theDay =(DateTime.Parse(reservationdate)).Day;
+                //query syntax
+                var results = from item in context.SpecialEvents
+                              orderby item.Description
+                              select new ReservationByDate()
+                              {
+                                    Description = item.Description,
+                                    Reservations = from r in item.Reservations
+                                                   where r.ReservationDate.Year == theYear
+                                                     && r.ReservationDate.Month == theMonth
+                                                     && r.ReservationDate.Day == theDay
+                                                    select new ReservationDetail() 
+                                                    {
+                                                        CustomerName = r.CustomerName,
+                                                        ReservationDate = r.ReservationDate,
+                                                        NumberInParty = r.NumberInParty,
+                                                        ContactPhone = r.ContactPhone,
+                                                        ReservationStatus = r.ReservationStatus,
+                                                    }
+                                                           
+
+                              };
+                return results.ToList();
+            }
+        }
+      
     }
 }
